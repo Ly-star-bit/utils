a9c7c575066417aa930bb88eb65ac30f9585d9ce
unknown <luoyu@bndxqc.com.cn>
2023-04-18 16:57:36 +0800

    delete all files

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .driver.dd_code import VK
-from .driver.dd_driver import DD_Driver_Mouse_Keybord
-from .driver import dd_driver
-from .driver import dd_code
--- driver/__init__.py
+++
@@ -1,2 +0,0 @@
-from .dd_code import VK
-from .dd_driver import DD_Driver_Mouse_Keybord
